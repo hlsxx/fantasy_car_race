09bf9aba72a49d24fd986782d7d6a22211c3a3c6
Holes70 <holespato@gmail.com>
2023-09-29 22:38:25 +0200

    player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,12 @@ public class Player : MonoBehaviour
     }
 
     private void Move() {
-        var deltaX = Input.GetAxis("Horizontal");
-        var newYPos = transform.position.y + deltaX;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
 
-        transform.position = new Vector2(transform.position.x, newYPos);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }
