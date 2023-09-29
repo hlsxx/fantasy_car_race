545e9f5198e3a136083c970957bc26e2e1dc047a
Holes70 <holespato@gmail.com>
2023-09-29 22:28:45 +0200

    player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,19 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       Move(); 
+    }
+
+    private void Move() {
+        var deltaX = Input.GetAxis("Horizontal");
+        var newYPos = transform.position.y + deltaX;
+
+        transform.position = new Vector2(transform.position.x, newYPos);
     }
 }
