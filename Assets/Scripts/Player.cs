ebef340c189be102f4e8adf9e8af53f263e68cc0
Holes70 <holespato@gmail.com>
2023-10-06 21:58:29 +0200

    ..

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float sidePadding = 1f;
     [SerializeField] GameObject projectilePrefab;
-    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileSpeed = 30f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         SetUpMoveBoundaries();    
+        StartCoroutine(TestCoroutine());
     }
 
     // Update is called once per frame
@@ -29,8 +33,9 @@ public class Player : MonoBehaviour
 
     private void Fire() {
         if (Input.GetButtonDown("Fire1")) {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;
-            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0);
+            StartCoroutine(FireContinously());
+        } else if (Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
         }
     }
 
@@ -52,5 +57,19 @@ public class Player : MonoBehaviour
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - sidePadding;
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + 5f;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - 11f;
+    }
+
+    IEnumerator TestCoroutine() {
+        Debug.Log("First part.");
+        yield return new WaitForSeconds(3);
+        Debug.Log("Second part.");
+    }
+
+    IEnumerator FireContinously() {
+        while(true) {
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 }
