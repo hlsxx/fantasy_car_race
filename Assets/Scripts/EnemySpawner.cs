eb009812651ddf49d50abdc0c9aaa7a2bc84a618
Holes70 <holespato@gmail.com>
2023-10-14 19:13:38 +0200

    ..

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -18,7 +18,12 @@ public class EnemySpawner : MonoBehaviour
         int tmp = waveConfig.GetNumberOfEnemies();
 
         for (int enemyCount = 0; enemyCount < tmp; enemyCount++) {
-            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(), 
+                waveConfig.GetWaypoints()[0].transform.position, 
+                Quaternion.identity
+            );
+
             newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
